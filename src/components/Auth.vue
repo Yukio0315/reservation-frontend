5e79687f67846378912565a79fe14cd387ac4c29
Yukio Ueda <triangle.pillow@gmail.com>
2020-04-24 02:19:06 +0900

    add vee-validate

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -21,26 +21,50 @@
     </v-toolbar>
     <v-card-text>
       <v-form>
-        <v-text-field
-          v-if="!signIn"
-          label="Name"
+        <ValidationProvider
+          v-slot="{ errors }"
           name="Name"
-          prepend-icon="mdi-account"
-          type="text"
-        />
-        <v-text-field
-          label="Email"
+          rules="required|max:10"
+        >
+          <v-text-field
+            v-if="!signIn"
+            v-model="name"
+            label="Name"
+            name="Name"
+            :error-messages="errors"
+            prepend-icon="mdi-account"
+            type="text"
+          />
+        </ValidationProvider>
+        <ValidationProvider
+          v-slot="{ errors }"
           name="Email"
-          prepend-icon="mdi-email"
-          type="text"
-        />
-        <v-text-field
-          id="password"
-          label="Password"
-          name="password"
-          prepend-icon="mdi-lock"
-          type="password"
-        />
+          rules="required|email"
+        >
+          <v-text-field
+            v-model="email"
+            label="Email"
+            name="Email"
+            :error-messages="errors"
+            prepend-icon="mdi-email"
+            type="text"
+          />
+        </ValidationProvider>
+        <ValidationProvider
+          v-slot="{ errors }"
+          name="Password"
+          rules="required|min:5|max:30"
+        >
+          <v-text-field
+            id="password"
+            v-model="password"
+            label="Password"
+            name="password"
+            :error-messages="errors"
+            prepend-icon="mdi-lock"
+            type="password"
+          />
+        </ValidationProvider>
       </v-form>
     </v-card-text>
     <v-card-actions>
@@ -52,12 +76,23 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import { extend } from "vee-validate";
+import { ValidationProvider } from "vee-validate";
+import { required, email, max, min } from "vee-validate/dist/rules";
 
-@Component
+extend("email", email);
+extend("required", required);
+extend("max", max);
+extend("min", min);
+
+@Component({ components: { ValidationProvider } })
 export default class Auth extends Vue {
   signIn = true;
   content = "Sign in";
   switchMessage = "Register";
+  name = "";
+  email = "";
+  password = "";
 
   toggleAuth() {
     const tmp = this.content;
